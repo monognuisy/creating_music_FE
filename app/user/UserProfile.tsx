db60f67ad3483765ef49b78500beddeceb4b7c99
losevanni <kms1202smdm@gmail.com>
2024-06-23 12:21:11 +0900

    [UPDATE] 로그인 이미지 로드 문제 해결

--- app/user/UserProfile.tsx
+++ app/user/UserProfile.tsx
@@ -1,16 +1,11 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 interface Props {
   getLoginStatus: boolean;
   getProfileIMG: string;
 }
 
 const UserProfile: React.FC<Props> = ({ getLoginStatus, getProfileIMG }) => {
-  var profile = sessionStorage.getItem("profile");
-  if(profile==null){
-    profile =
-      "https://static-00.iconduck.com/assets.00/profile-default-icon-512x511-v4sw4m29.png";
-  }
   if (getLoginStatus == false) {
     return null;
   } else {
@@ -19,7 +14,7 @@ const UserProfile: React.FC<Props> = ({ getLoginStatus, getProfileIMG }) => {
         <div className="h-12 w-12 overflow-hidden rounded-full">
           <a href="/userinfo">
             <img
-              src={profile}
+              src={getProfileIMG}
               alt="tmp"
               className="h-full w-full object-cover"
             />
