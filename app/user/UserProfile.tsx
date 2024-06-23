f63468fe955577bc16493deab317c287d7b726a6
losevanni <kms1202smdm@gmail.com>
2024-06-23 11:57:57 +0900

    [UPDATE] 로그인시 프로필 이미지 로딩

--- app/user/UserProfile.tsx
+++ app/user/UserProfile.tsx
@@ -6,6 +6,11 @@ interface Props {
 }
 
 const UserProfile: React.FC<Props> = ({ getLoginStatus, getProfileIMG }) => {
+  var profile = sessionStorage.getItem("profile");
+  if(profile==null){
+    profile =
+      "https://static-00.iconduck.com/assets.00/profile-default-icon-512x511-v4sw4m29.png";
+  }
   if (getLoginStatus == false) {
     return null;
   } else {
@@ -14,7 +19,7 @@ const UserProfile: React.FC<Props> = ({ getLoginStatus, getProfileIMG }) => {
         <div className="h-12 w-12 overflow-hidden rounded-full">
           <a href="/userinfo">
             <img
-              src={getProfileIMG}
+              src={profile}
               alt="tmp"
               className="h-full w-full object-cover"
             />
